bba46583364729d9add0d6cf89f5f1fc3acfec36
suntong233 <2654073941@qq.com>
2019-10-23 14:22:15 +0800

    st

---
+++ src/components/js/Variablesandtypes.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        变量和类型
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Js.vue
+++ src/views/Js.vue
@@ -1,21 +1,66 @@
 <template>
-  <div>
+  <div class="Js-container">
       <HeaderNav></HeaderNav>
-      Js
+      <Catalog :showitem="showComponent" @handleChange="handleChange" :cataloglist="componentsData"></Catalog>
+      <div class="main-container">
+          <component :is="showComponent"></component>
+      </div>
   </div>
 </template>
 
 <script>
 import HeaderNav from "../components/public/HeaderNav"; // 引入顶部组件
+import Catalog from "../components/public/Catalog"; // 引入目录组件
+import VarandType from "../components/js/Variablesandtypes"; // 引入变量和类型
+import { _saveLocal } from "../utils/public.js"; // 引入本地存储工具
 const components = {
-    HeaderNav
+    HeaderNav,
+    Catalog,
+    VarandType
 }
 export default {
     name: "Js",
+    data () {
+        return {
+            showComponent: 'VarandType',
+            componentsData: [
+                {   // 变量和类型
+                    name: "VarandType"
+                }
+            ],
+        }
+    },
+    methods: {
+        handleChange(e){
+            this.showComponent = e
+            _saveLocal('jsshowComponent',this.showComponent)
+        }
+    },
+    created () {
+        if(localStorage['jsshowComponent']){
+            this.showComponent = JSON.parse(localStorage['jsshowComponent'])
+        }
+    },
     components
 }
 </script>
 
 <style scoped>
-
+    .Js-container{
+        width: 100%;
+    }
+    .main-container{
+        margin: 10px 0 0 300px;
+    }
+    @media only screen and (min-width: 960px) {
+        .main-container{
+            width: 960px;
+        }
+    }
+    @media only screen and (max-width: 960px) {
+        .main-container{
+            width: 100%;
+            margin: 26px 0;
+        }
+    }
 </style>
